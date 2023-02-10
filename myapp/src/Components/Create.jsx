1e549d0e784a24ff1275002125450b05dcac3511
Ashutosh5333 <lakshakarashutosh@gmail.com>
2023-02-10 10:56:48 +0530

    create post is updated

--- myapp/src/Components/Create.jsx
+++ myapp/src/Components/Create.jsx
@@ -1,18 +1,8 @@
 import React from 'react'
-import {
-    Modal,
-    ModalOverlay,
-    ModalContent,
-    ModalHeader,
-    ModalFooter,
-    ModalBody,
-    ModalCloseButton,
-    Box,
-    Text,
-    Button,
-    useDisclosure,
-  } from '@chakra-ui/react'
+import {Modal, ModalOverlay, ModalContent, ModalHeader, ModalFooter, ModalBody, ModalCloseButton,  Box,  Text,Button, useDisclosure,Input, Switch, Divider,} from '@chakra-ui/react'
 import { MdOutlineCreateNewFolder } from 'react-icons/md'
+import {TiTickOutline} from "react-icons/ti"
+import {BiArrowBack} from "react-icons/bi"
 
 const Create = () => {
 
@@ -27,25 +17,165 @@ const Create = () => {
        <MdOutlineCreateNewFolder/> 
        </Box> 
 
-      {/* <Text  onClick={onOpen}>
-      <MdOutlineCreateNewFolder/> 
-      </Text> */}
-             
+    
+       {/* width={{ base: "40%", md: "80%", lg: "90%" }} */}
 
-      <Modal isOpen={isOpen} onClose={onClose}>
+      <Modal isOpen={isOpen} onClose={onClose} size="2xl" >
         <ModalOverlay />
-        <ModalContent>
-          <ModalHeader>Modal Title</ModalHeader>
-          <ModalCloseButton />
-          <ModalBody>
-            lorem
+
+        <ModalContent  >
+          {/* <ModalHeader>Modal Title</ModalHeader> */}
+           <Box 
+          //  border={"1px solid red"} 
+           display="flex" justifyContent={"space-between"}>
+            <Box fontSize={"35px"}  >
+            <BiArrowBack/>
+            </Box>
+              <Text fontWeight={"600"} fontSize="25px" fontFamily={"sans-serif"} fontStyle={"italic"} > New Post  </Text>
+             <Box fontSize={"35px"} color="blue">
+            <TiTickOutline  />
+             </Box>
+           </Box>
+
+          <ModalBody >
+            <Box 
+            // border={"1px solid red"}
+             >
+
+              <Box 
+              //  border="1px solid black" 
+                 height="80px" >  
+                 <Input placeholder='Write a caption' height="80px"  
+                 border="none" />
+               </Box>
+              <Box border="1px solid black" > 
+               
+              </Box>
+               {/* ------------------- */}
+
+              <Box 
+              // border="1px solid blue"
+              >
+               
+              
+              <Box textAlign={"left"}     >
+               <Text fontWeight="600" p={3}> Tag people </Text>
+               <Divider orientation='horizontal' />
+               <Text fontWeight="600" p={3}> Add message button </Text>
+               <Divider orientation='horizontal' />
+               <Text p={3} fontWeight="600" > Add Location </Text>
+               <Divider orientation='horizontal' />
+               <Text p={3}  fontWeight="600" > Add Reminder </Text>
+               <Divider orientation='horizontal' />
+               <Text p={3}  fontWeight="600" >Add music</Text>
+               <Divider orientation='horizontal' />
+             </Box>
+
+              </Box>
+
+        {/* -------------------- bottom ------------- */}
+
+              <Box 
+              // border="1px solid yellow"
+              > 
+              
+              <Box
+              // border="1px solid green"
+              display="flex" fontWeight="600" justifyContent={"space-between"} p={4} >
+                     <Box 
+                    //  border="1px solid brown"
+                     > 
+                     <Text  >
+                       Boost post
+                     </Text> </Box>
+                      <Box 
+                      // border="1px solid yellow"
+                      >
+                      <Switch id='email-alerts' />
+                      </Box>
+               </Box>
+
+              </Box>
+              <Divider orientation='horizontal' />
+          {/* ---------------------- */}
+          <Box 
+          // border="1px solid green"
+          display="flex"  fontWeight="600" justifyContent={"space-between"} p={4} >
+                     <Box 
+                    //  border="1px solid brown"
+                     > 
+                     <Text>
+                      Share to Facebook
+                     </Text> </Box>
+                      <Box 
+                      // border="1px solid yellow"
+                      >
+                      <Switch id='email-alerts' />
+                      </Box>
+               </Box>
+               <Box 
+              // border="1px solid green"
+              
+                display="flex"  fontWeight="600" justifyContent={"space-between"} p={4} >
+                     <Box 
+                    //  border="1px solid brown"
+                     > 
+                     <Text>
+                      Share to Twitter
+                     </Text> </Box>
+                      <Box 
+                      // border="1px solid yellow"
+                      >
+                      <Switch id='email-alerts' />
+                      </Box>
+               </Box>
+               <Box 
+              // border="1px solid green"
+               
+               display="flex"  fontWeight="600" justifyContent={"space-between"} p={4} >
+                     <Box 
+                    //  border="1px solid brown"
+                     > 
+                     <Text>
+                     Share to Tumblr
+                     </Text> </Box>
+                      <Box 
+                      // border="1px solid yellow"
+                      >
+                      <Switch id='email-alerts' />
+                      </Box>
+               </Box>
+               <Box 
+              //  
+              // border="1px solid green"
+               
+               display="flex"  fontWeight="600" justifyContent={"space-between"} p={4} >
+                     <Box 
+                    //  border="1px solid brown"
+                     > 
+                     <Text>
+                       Advanced settings 
+                     </Text> </Box>
+                      <Box 
+                      // border="1px solid yellow"
+                      >
+                      <Switch id='email-alerts' />
+                      </Box>
+               </Box>
+
+
+
+            </Box>
           </ModalBody>
 
           <ModalFooter>
-            <Button colorScheme='blue' mr={3} onClick={onClose}>
+            
+            {/* <Button colorScheme='blue' mr={3} onClick={onClose}>
               Close
-            </Button>
-            <Button variant='ghost'>Secondary Action</Button>
+            </Button> */}
+            
+            {/* <Button variant='ghost'>Secondary Action</Button> */}
+
           </ModalFooter>
         </ModalContent>
       </Modal>
